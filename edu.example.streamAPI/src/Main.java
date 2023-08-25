d34fc3a052ce0e3a61d05860a19906052a509738
Michael Kevin <michaelkmb@hotmail.com>
2023-08-25 20:26:56 -0300

    Desafio 3: verificar número negativo;

--- edu.example.streamAPI/src/Main.java
+++ edu.example.streamAPI/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static java.util.stream.StreamSupport.stream;
@@ -12,10 +13,15 @@ public class Main {
         return (list.stream().filter(n -> n%2 == 0).toList()).stream().reduce(0, Integer::sum);
     }
 
+    public static boolean verificaSeTodosSaoPositivos(List<Integer> list) {
+        return list.stream().anyMatch(n -> n<0);
+    }
+
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
         System.out.println(ordenaLista(numeros));
         System.out.println(somaParesLista(numeros));
+        System.out.println("Todos da lista são  positivos? " + (verificaSeTodosSaoPositivos(numeros) ? "Não" : "Sim"));
     }
 }
