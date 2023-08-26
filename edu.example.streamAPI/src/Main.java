8fe84182e75092b146c52059ac31ffe618ac7898
Michael Kevin <michaelkmb@hotmail.com>
2023-08-26 12:24:37 -0300

    Desafio 11: soma dos quadrados;

--- edu.example.streamAPI/src/Main.java
+++ edu.example.streamAPI/src/Main.java
@@ -59,6 +59,10 @@ public class Main {
         return agrupado.stream().distinct().sorted().collect(toList());
     }
 
+    public static List<Integer> quadrados(List<Integer> list) {
+         return list.stream().map(n -> n*n).collect(toList());
+    }
+
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
@@ -73,5 +77,6 @@ public class Main {
         System.out.println("Soma de todos os dígitos: " + somaTodosDigitos(numeros));
         System.out.println("Todos são distintos? " + (todosDistintos(numeros) ? "Sim" : "Não"));
         System.out.println("Agrupar múltiplos de 3 e 5: " + juntarMultiplosDes10(numeros));
+        System.out.println("Soma dos quadrados: " + quadrados(numeros).stream().reduce(0, Integer::sum));
     }
 }
